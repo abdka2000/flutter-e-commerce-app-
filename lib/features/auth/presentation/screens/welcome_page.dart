c24d03b5cb456c8b5f1bba0103f5de9a74d57de1
abdka2000 <eww004435@gmail.com>
2024-02-16 10:49:22 -0800

    refactor welcome page body

--- lib/features/auth/presentation/screens/welcome_page.dart
+++ lib/features/auth/presentation/screens/welcome_page.dart
@@ -1,43 +1,15 @@
-import 'package:e_commerce/features/auth/presentation/widgets/welcome_page_register_section.dart';
+import 'package:e_commerce/features/auth/presentation/widgets/welcome_page_body.dart';
 import 'package:flutter/material.dart';
-
-import '../widgets/cosy_image.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(children: [
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            // ignore: sized_box_for_whitespace
-            Container(
-              width: double.infinity,
-              child: Image.asset(
-                "assets/images/main image.png",
-                fit: BoxFit.cover,
-              ),
-            ),
-            const COSYImage(),
-            const Text(
-              "Add some coziness\n to your home",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                  height: 0.9,
-                  color: Colors.white,
-                  fontSize: 35,
-                  fontWeight: FontWeight.w100),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const WelcomePageRegisterSection(),
-          ],
-        ),
-      ]),
+    return const Scaffold(
+      body: SafeArea(
+        child: WelcomePageBody(),
+      ),
     );
   }
 }
---
+++ lib/features/auth/presentation/widgets/welcome_page_body.dart
@@ -0,0 +1,43 @@
+
+import 'package:e_commerce/features/auth/presentation/widgets/cosy_image.dart';
+import 'package:e_commerce/features/auth/presentation/widgets/welcome_page_register_section.dart';
+import 'package:flutter/material.dart';
+
+class WelcomePageBody extends StatelessWidget {
+  const WelcomePageBody({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(children: [
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          // ignore: sized_box_for_whitespace
+          Container(
+            width: double.infinity,
+            child: Image.asset(
+              "assets/images/main image.png",
+              fit: BoxFit.cover,
+            ),
+          ),
+          const COSYImage(),
+          const Text(
+            "Add some coziness\n to your home",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                height: 0.9,
+                color: Colors.white,
+                fontSize: 35,
+                fontWeight: FontWeight.w100),
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          const WelcomePageRegisterSection(),
+        ],
+      ),
+    ]);
+  }
+}
